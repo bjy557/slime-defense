e8befd73638f4bbb31f829a2ded990f7f381268e
bjy557 <bjy557@naver.com>
2024-03-16 23:32:54 +0900

    SPUM 모듈 추가

--- Assets/Slime Defense/codes/Player.cs
+++ Assets/Slime Defense/codes/Player.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
+    private Vector3 moveVector = Vector3.zero;
     public float speed;
 
     Rigidbody2D rigid;
+
+    private Animator animator;
+
+    private Transform transform;
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +22,49 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveVector.x = moveX;
+        moveVector.y = moveY;
+
+        transform.Translate(moveVector.normalized * Time.deltaTime * 5f);
+
+        if (moveX > 0)
+        {
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+        else if (moveX < 0)
+        {
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (moveX != 0 || moveY != 0)
+        {
+            animator.SetFloat("RunState", 0.5f);
+        }
+        else
+        {
+            animator.SetFloat("RunState", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("Die");
+        }
     }
 
     private void FixedUpdate()
