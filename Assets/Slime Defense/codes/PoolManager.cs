28094812d5a77a7a28d48dcf79f7b24362fcfd07
bjy557 <bjy557@naver.com>
2024-03-18 00:56:25 +0900

    enemy spawner 설정

--- Assets/Slime Defense/codes/PoolManager.cs
+++ Assets/Slime Defense/codes/PoolManager.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    
+    public GameObject[] prefabs;
+
+    List<GameObject>[] pools;
+
+    private void Awake()
+    {
+        pools = new List<GameObject>[prefabs.Length];
+
+        for (int i=0; i < pools.Length; i++)
+        {
+            pools[i] = new List<GameObject>();
+        }
+
+        Debug.Log(pools.Length);
+    }
+
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+
+        // 선택한 pool의 비활성화 object 가져옴
+        foreach (GameObject item in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        // 못찾은 경우 새롭게 생성해서 select 할당
+        if (!select)
+        {
+            select = Instantiate(prefabs[index], transform);
+            pools[index].Add(select);
+        }
+
+        return select;
+    }
 }
