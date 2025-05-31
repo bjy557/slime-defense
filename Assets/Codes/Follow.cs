80a8e0e684e44756a3d30d82caebc059f17480c4
noah <bjy557@naver.com>
2025-05-31 14:54:14 +0900

    플레이어 위치 조정 + 밀쳐지지 않도록 kinematic 설정 추가

--- Assets/Codes/Follow.cs
+++ Assets/Codes/Follow.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     RectTransform rect;
+    public Vector3 offset = new Vector3(-0.05f, 0.06f, 0);
 
     void Awake()
     {
@@ -11,6 +12,7 @@ public class Follow : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rect.position = Camera.main.WorldToScreenPoint(GameManager.instance.player.transform.position);
+        rect.position = Camera.main.WorldToScreenPoint(GameManager.instance.player.transform.position + offset);
+        rect.localScale = Vector3.one * 0.8f;
     }
 }
